782abebfe285f07997ed4c414b1bbae9f26e1598
MinaAashraf <ashrafmina444@gmail.com>
2022-03-18 01:56:48 +0200

    add update handler endpoint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const products_controller = require('./products-controller')
 
 const app = express();
 app.use(bodyParser.json())
- console.log('mina')
-// TODO -> end points:
 
+// update 
+app.put('/products/:code',products_controller.updateHandler)
 
 app.listen(8080,()=> console.log('Server running'))
--- products-controller.js
+++ products-controller.js
@@ -1,21 +1,30 @@
 // products list (DB)
 let products = [{
     smallDescription : 'Andora Trendy Plaids Full Sleeves Shirt - Baby Blue & White',
-    code: '#54a24',
+    code: '2501',
     price: 149.00,
     category: 'Fashion'
 },
 {
     smallDescription: 'Infinix Smart 6 - 6.6-inch 64GB/3GB Dual SIM Mobile Phone-Quetzal Cyan',
-    code: '#14234',
+    code: '2502',
     price: 2074.00,
     category:'Phones & Tablets'
 }
 ]
 
-// TODO -> end points handlers:
-
-
+// update Handler
+const updateHandler = (req,res)=>{
+   var index = products.findIndex((product) => product.code === req.params.code)
+   // check if product exist or not
+   if (index !== -1)
+    {
+        products[index] = req.body
+        res.send(products[index]) 
+    }
+    else
+      res.status(404).send({error: 'product code is not found!'})
+}
 
 // export handlers
-module.exports = {}
+module.exports = {updateHandler}
