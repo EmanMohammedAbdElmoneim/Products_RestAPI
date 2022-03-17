cbc2747f1bbef250c0623d4a4f1d4b91a7fa29f4
Mina Ashraf <48159614+MinaAashraf@users.noreply.github.com>
2022-03-18 01:14:03 +0200

    Update products-controller.js

--- products-controller.js
+++ products-controller.js
@@ -1,13 +1,13 @@
 // products list (DB)
 let products = [{
     smallDescription : 'Andora Trendy Plaids Full Sleeves Shirt - Baby Blue & White',
-    code: '#54a24',
+    code: '2501',
     price: 149.00,
     category: 'Fashion'
 },
 {
     smallDescription: 'Infinix Smart 6 - 6.6-inch 64GB/3GB Dual SIM Mobile Phone-Quetzal Cyan',
-    code: '#14234',
+    code: '2502',
     price: 2074.00,
     category:'Phones & Tablets'
 }
