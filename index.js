7341f9cb8fdc5ee057127be56f00e2cd68ed2489
MinaAashraf <ashrafmina444@gmail.com>
2022-03-17 20:11:37 +0200

    add necessary dependencies & files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const products_controller = require('./products-controller')
+
+const app = express();
+app.use(bodyParser.json())
+ console.log('mina')
+// TODO -> end points:
+
+
+app.listen(8080,()=> console.log('Server running'))
---
+++ products-controller.js
@@ -0,0 +1,21 @@
+// products list (DB)
+let products = [{
+    smallDescription : 'Andora Trendy Plaids Full Sleeves Shirt - Baby Blue & White',
+    code: '#54a24',
+    price: 149.00,
+    category: 'Fashion'
+},
+{
+    smallDescription: 'Infinix Smart 6 - 6.6-inch 64GB/3GB Dual SIM Mobile Phone-Quetzal Cyan',
+    code: '#14234',
+    price: 2074.00,
+    category:'Phones & Tablets'
+}
+]
+
+// TODO -> end points handlers:
+
+
+
+// export handlers
+module.exports = {}
