6e8c287c60e284c000705cd9bc8ba6a1e43dc532
EmanMohammedAbdElmoneim <eman170251@feng.bu.edu.eg>
2022-03-18 02:42:03 +0200

    git push origin deleteProduct

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const products_controller = require('./products-controller')
 const app = express();
 app.use(bodyParser.json())
 // TODO -> end points:
-
+app.delete("/products/:id", products_controller.deleteProduct);
 
 app.listen(8080,()=> console.log('Server running'))
