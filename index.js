ce392667507ea6fe79ca2ab5b1a51124a8f63823
MinaAashraf <ashrafmina444@gmail.com>
2022-03-18 03:40:31 +0200

    add getByCode endpoint

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const products_controller = require('./products-controller')
 
 const app = express();
 app.use(bodyParser.json())
-// TODO -> end points:
 
+
+// getByCode endpoint
+app.get('/products/:code',products_controller.getByCode)
 
 app.listen(8080,()=> console.log('Server running'))
--- products-controller.js
+++ products-controller.js
@@ -1,21 +1,27 @@
 // products list (DB)
 let products = [{
     smallDescription : 'Andora Trendy Plaids Full Sleeves Shirt - Baby Blue & White',
-    code: '2501',
+    code: '1',
     price: 149.00,
     category: 'Fashion'
 },
 {
     smallDescription: 'Infinix Smart 6 - 6.6-inch 64GB/3GB Dual SIM Mobile Phone-Quetzal Cyan',
-    code: '2502',
+    code: '2',
     price: 2074.00,
     category:'Phones & Tablets'
 }
 ]
 
-// TODO -> end points handlers:
-
-
+// get product by code
+const getByCode =(req,res)=>{
+  var index = products.findIndex((product)=> product.code === req.params.code)
+  //check if product code is exist
+  if (index !== -1)
+      res.send(products[index])
+  else
+     res.status(404).send({error: 'product code is not found'}) 
+}
 
 // export handlers
-module.exports = {}
+module.exports = {getByCode}
