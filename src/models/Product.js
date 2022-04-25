a8e2eccb780afdf546e69bbc7ea3e6eefe1a6d3d
unknown <manareldredy@gmail.com>
2022-04-25 21:44:47 +0200

    schema build

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,2 +1,38 @@
-const mongoose = require('mongoose')
+const {Schema, model} = require('mongoose');
+const productSchema = new Schema({
+    description: {
+        type: String,
+        rquired: true,
+    },
+    category: {
+        type: String,
+        rquired: true,
+        enum: ['shirts', 'shoes'],
+        default: 'shirts',
+    },
+    price: {
+         type: Number,
+         required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+        enum: ['black', 'white', 'red', 'dark blue', 'blue', 'grey'],
+        default: 'black',
+    },
+    size: {
+        type: [String],
+        required: true,
+    },
+    brand: {
+    type: String,
+    required: false,
+    },
 
+});
+
+//create model
+const User = model('Product', productSchema);
+module.exports = {
+    Product,
+};
