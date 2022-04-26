f70351828ad838e0173730eafb77c2a0873702a3
MinaAashraf <ashrafmina444@gmail.com>
2022-04-26 03:54:40 +0200

    add findById method

--- src/controllers/Product_Controller.js
+++ src/controllers/Product_Controller.js
@@ -3,7 +3,16 @@ const { Product } = require('../models/Product');
 
  const insertProduct = (req, res)=>{};
  const getAllProducts =(req , res) => {};
- const getById =async (req,res)=>{};
+ const getById =async (req,res)=>{
+    try{ 
+        const id =  req.params.id
+        const product = await Product.findById({_id : id})
+        res.status(200).json(product)
+    }
+    catch(err){  
+         res.status(400).json(err)
+    }
+ };
  const filterByBrand =async (req,res)=>{};
  const sortDescendingly = async(req , res) =>{};
  const sortAssendingly = async(req, res) =>{};
